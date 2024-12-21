216c1943722579f219e7694cdf9e52639e0148fc
Michał Postek <postek.michal@protonmail.com>
2024-12-21 09:35:55 +0100

    move Node to the main file

--- src/App.java
+++ src/App.java
@@ -1,6 +1,21 @@
 import java.io.File;
 import java.util.*;
 
+class Node {
+    int row, col, g;
+    double h, f;
+    Node previous;
+
+    public Node(int row, int col, int g, double h, Node previous) {
+        this.row = row;
+        this.col = col;
+        this.g = g;
+        this.h = h;
+        this.f = g + h;
+        this.previous = previous;
+    }
+}
+
 public class App {
     static final int MAP_SIZE = 20;
     static final int START_ROW = MAP_SIZE - 1;
